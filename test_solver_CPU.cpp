9082d423df4200c275c99b32360865b66cc4bc34
Vasilis Naserentin <vasilis.naserentin@chalmers.se>
2025-04-18 19:48:16 +0000

    Added AMGCL tests.

---
+++ test_solver_CPU.cpp
@@ -0,0 +1,83 @@
+#include <vector>
+#include <iostream>
+
+#include <amgcl/backend/builtin.hpp>
+#include <amgcl/adapter/crs_tuple.hpp>
+#include <amgcl/make_solver.hpp>
+#include <amgcl/amg.hpp>
+#include <amgcl/coarsening/smoothed_aggregation.hpp>
+#include <amgcl/relaxation/spai0.hpp>
+#include <amgcl/solver/bicgstab.hpp>
+#include <amgcl/solver/gmres.hpp>
+#include <amgcl/io/mm.hpp>
+#include <amgcl/profiler.hpp>
+
+int main(int argc, char *argv[]) {
+    // The matrix and the RHS file names should be in the command line options:
+    if (argc < 3) {
+        std::cerr << "Usage: " << argv[0] << " <matrix.mtx> <rhs.mtx>" << std::endl;
+        return 1;
+    }
+
+    // The profiler:
+    amgcl::profiler<> prof("poisson3Db");
+
+    // Read the system matrix and the RHS:
+    ptrdiff_t rows, cols;
+    std::vector<ptrdiff_t> ptr, col;
+    std::vector<double> val, rhs;
+
+    prof.tic("read");
+    std::tie(rows, cols) = amgcl::io::mm_reader(argv[1])(ptr, col, val);
+    std::cout << "Matrix " << argv[1] << ": " << rows << "x" << cols << std::endl;
+
+    std::tie(rows, cols) = amgcl::io::mm_reader(argv[2])(rhs);
+    std::cout << "RHS " << argv[2] << ": " << rows << "x" << cols << std::endl;
+    prof.toc("read");
+    
+    // We use the tuple of CRS arrays to represent the system matrix.
+    // Note that std::tie creates a tuple of references, so no data is actually
+    // copied here:
+    auto A = std::tie(rows, ptr, col, val);
+
+    // Compose the solver type
+    //   the solver backend:
+    typedef amgcl::backend::builtin<double> SBackend;
+    //   the preconditioner backend:
+    typedef amgcl::backend::builtin<double> PBackend;
+    
+    typedef amgcl::make_solver<
+        amgcl::amg<
+            PBackend,
+            amgcl::coarsening::smoothed_aggregation,
+            amgcl::relaxation::spai0
+            >,
+        amgcl::solver::gmres<SBackend>
+        > Solver;
+    
+    Solver::params prm;
+    prm.solver.tol = 1e-6;
+    std::cout<< "Initializing solver"<< std::endl;
+    // Initialize the solver with the system matrix:
+    prof.tic("setup");
+    Solver solve(A,prm);
+    prof.toc("setup");
+
+    // Show the mini-report on the constructed solver:
+    std::cout << solve << std::endl;
+
+    // Solve the system with the zero initial approximation:
+    int iters;
+    double error;
+    std::vector<double> x(rows, 0.0);
+
+    prof.tic("solve");
+    std::tie(iters, error) = solve(A, rhs, x);
+    prof.toc("solve");
+
+    // Output the number of iterations, the relative error,
+    // and the profiling data:
+    std::cout << "Iters: " << iters << std::endl
+              << "Error: " << error << std::endl
+              << prof << std::endl;
+}
