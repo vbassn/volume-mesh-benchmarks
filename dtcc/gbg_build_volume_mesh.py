ff950acadc6f457230dc3fc8412d871177b74ef5
georspai <spaikgeo@gmail.com>
2025-06-18 17:46:13 +0300

    Boundary Face Marker processing

--- dtcc/gbg_build_volume_mesh.py
+++ dtcc/gbg_build_volume_mesh.py
@@ -2,13 +2,14 @@
 
 import dtcc
 from build_volume_mesh import build_volume_mesh
+from utils import extract_meshes_from_boundary_markers,save_mesh_with_boundary_markers
 
 # Poseidon (57.6971779, 11.9795910)
 x0 = 319995.962899
 y0 = 6399009.716755
-L = 200.0
-H = 100.0
-h = 5.0
+L = 100.0
+H = 50.0
+h = 2.5
 
 # Define bounds
 bounds = dtcc.Bounds(x0 - 0.5 * L, y0 - 0.5 * L, x0 + 0.5 * L, y0 + 0.5 * L)
@@ -19,8 +20,13 @@ buildings = dtcc.download_footprints(bounds=bounds)
 
 # Build volume mesh
 volume_mesh = build_volume_mesh(pointcloud, buildings, H, h)
-print(volume_mesh)
+
+# Extract and save boundary meshes
+extract_meshes_from_boundary_markers(volume_mesh, volume_mesh.boundary_markers)
+
+
 
 # Save mesh to file
 volume_mesh.save("gbg_volume_mesh.vtu")
 volume_mesh.save("gbg_volume_mesh.xdmf")
+volume_mesh.save_boundary_markers("gbg_volume_mesh_with_boundary_markers.vtu")
